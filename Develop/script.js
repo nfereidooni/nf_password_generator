dcdaa93afd398c8b2664832d55402cd8184be1df
Niki Fereidooni <nikifereidooni@gmail.com>
2020-09-21 00:33:34 -0400

    Added pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,23 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log('Function is running')
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
+
+// Ask how many charcters user would like password to contain
+
+// Confirm if user would like special characters
+
+// Confirm if user wants numeric characters
+
+// Confirm if user wants lowercase characters
+
+// Confirm if user wants uppercase characters
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
